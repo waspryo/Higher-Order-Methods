3f0d9dbbe5019f4ffa829d1dce83dcadf219c9a4
waspryo <wasp2123321@gmail.com>
2019-12-15 16:34:21 +0900

    invoke with different greetings

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ const greeting = (helloFn, name) => {
 
 greeting(sayHello, 'javaScript')
 
+//  Return a function
+const sayHelloWorld = function () {
+    return () => {
+        console.log('Hello!')
+    }
+}
+const innerFn = sayHelloWorld();
+innerFn()
+
+// 1. Store the return value in variable and involke it:
+
+// ```
+// const loghello = sayHello();
+// logHell();
+// ```
+
+// 3. Invoke the return value on the same line:
+
+// ```
+// sayHello()();
+// ```
+
+function getName() {
+    const name = 'Tony'
+    // console.log(name)
+    return name;
+}
+function greet(getNameFn, greeting) {
+    console.log(`${greeting}, ${getNameFn()}`);
+}
+// const greet = (getNameFn, greeting) => console.log(`${greeting} ${getNameFn()}`);
+greet(getName, 'hello')
+// console.log(greet)
