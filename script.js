ffc731c4c46c1efb91634e96e7c44967f5e9eac2
waspryo <wasp2123321@gmail.com>
2019-12-15 16:02:14 +0900

    pass a function as an argument

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// Assign a function to a variable
+
 const whatTimeIsIt = () => {
     console.log('It is:', new Date().toTimeString())
 }
 whatTimeIsIt();
+
+// pass a function as an argument
+
+const sayHello = () => {
+ return 'hello, '
+}
+const greeting = (helloFn, name) => {
+    console.log(helloFn() + name)
+}
+
+// const greeting = function (helloFn, name) {
+//     console.log(helloFn() + name)
+// }
+
+greeting(sayHello, 'javaScript')
 
